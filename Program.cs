588f1b03bf4c16c03c3eabce2095df110c360e06
ArthurEOrtiz <ortiz.arthur.e@gmail.com>
2022-08-07 12:10:26 -0600

    Initial Commit
    Basic project lay out for a password checker to practice C sharp code

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PasswordChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "%TGB5tgb";
+            char[] passwordArray = new char[password.Length];
+
+            for (int i = 0; i<password.Length; i++)
+            {
+                passwordArray[i] = password[i];
+            }
+
+            foreach (char c in passwordArray) {
+                Console.WriteLine(c);
+            }
+
+            
+        }
+    }
+}
