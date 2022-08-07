35531cf5aa1e73c28b9b3729144cc224102fe348
ArthurEOrtiz <ortiz.arthur.e@gmail.com>
2022-08-07 13:58:58 -0600

    Can iterate and check for specs on password

--- Program.cs
+++ Program.cs
@@ -8,17 +8,26 @@ namespace PasswordChecker
         {
             string password = "%TGB5tgb";
             char[] passwordArray = new char[password.Length];
-
+            int upperCaseCount = 0;
+            
+            
             for (int i = 0; i<password.Length; i++)
             {
                 passwordArray[i] = password[i];
             }
-
-            foreach (char c in passwordArray) {
-                Console.WriteLine(c);
-            }
-
             
+            foreach (char c in passwordArray)
+            {
+                bool upperCase = Char.IsUpper(c);
+                if (upperCase) 
+                {
+                    upperCaseCount++;
+                }
+                
+            }
+            Console.WriteLine(upperCaseCount);
+
+        
         }
     }
 }
